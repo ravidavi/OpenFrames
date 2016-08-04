0bd41c0858ca7096f1176e263868d34dfe633208
Ravi Mathur <ravi.mathur@emergentspace.com>
2016-08-04 00:17:37 -0400

    Append "X11" or "Cocoa" to default install folder name on OSX Winteracter demo now shows a satellite orbiting the Earth

--- Demos/Winteracter Test/trivial.f90
+++ Demos/Winteracter Test/trivial.f90
@@ -55,35 +55,63 @@
 	! Create a Winteracter window
 	IWIDTH = 800
 	IHEIGHT = 600
+	CALL WindowOpen(X=20, Y=20, WIDTH=IWIDTH, HEIGHT=IHEIGHT, TITLE='Trivial OpenGL Example')
 
 ! Create a WindowProxy which will draw onto the window
 	CALL OFWin_CreateProxy(0, 0, IWIDTH, IHEIGHT, 1, 1, .TRUE., 0)
-	CALL WindowOpen(X=20, Y=20, WIDTH=IWIDTH, HEIGHT=IHEIGHT, TITLE='Trivial OpenGL Example')
-	
+
+! Create a trajectory to hold orbit points
 	CALL OFTraj_Create("Trajectory", 3, 0)
-	
+
+! Create artist to draw orbit line
 	CALL OFCurveArtist_Create("CurveArtist")
 	CALL OFTrajArtist_SetTrajectory()
-	CALL OFCurveArtist_SetColor(1.0, 0.0, 0.0)
+	CALL OFCurveArtist_SetColor(1.0, 0.0, 0.0) ! Red
 	CALL OFCurveArtist_SetXData(OF_POSOPT, OF_X, 0, 1.0)
 	CALL OFCurveArtist_SetYData(OF_POSOPT, OF_Y, 0, 1.0)
 	CALL OFCurveArtist_SetZData(OF_POSOPT, OF_Z, 0, 1.0)
 	
-! Create a ReferenceFrame that will follow along the trajectory
-	CALL OFFrame_Create("Follower")
+! Create a satellite model that will follow along the trajectory
+        CALL OFModel_Create("Satellite")
+        CALL OFModel_SetModel("../Models/Hubble.3ds")
+        CALL OFModel_SetModelScale(0.001, 0.001, 0.001) ! 5meter size
 	CALL OFFrame_FollowTrajectory("Trajectory")
 	CALL OFFrame_FollowType(OFFOLLOW_POSITION, OFFOLLOW_LOOP)
 	CALL OFFrame_FollowPosition(src, element, opt, scale)
 
-! Create a DrawableTrajectory that will contain the artists as well as the following frame
+! Create the Earth
+        CALL OFSphere_Create("Earth")
+        CALL OFSphere_SetRadius(6371.0d0)
+        CALL OFSphere_SetTextureMap("../Images/EarthTexture.bmp")
+
+! Create a DrawableTrajectory to hold the orbit artist
 	CALL OFDrawTraj_Create("DrawTraj")
 	CALL OFDrawTraj_AddArtist("CurveArtist")
-	CALL OFFrame_AddChild("Follower")
+        CALL OFFrame_ShowAxes(OF_NOAXES)
+        CALL OFFrame_ShowAxesLabels(OF_NOAXES)
+        CALL OFFrame_ShowNameLabel(.false.)
+
+! Set up the Reference Frame Hierarchy
+        CALL OFFrame_Activate("Earth")
+        CALL OFFrame_AddChild("DrawTraj")
+	CALL OFFrame_AddChild("Satellite")
+
+! Set up a view of the Earth
+        CALL OFView_Create("Earth")
+        CALL OFView_SetViewFrame("Earth", "Earth")
+        CALL OFWin_AddView(0, 0)
+        CALL OFView_Reset()
+
+! Set up a view of the satellite
+        CALL OFView_Create("Satellite")
+        CALL OFView_SetViewFrame("Earth", "Satellite")
+        CALL OFWin_AddView(0, 0)
+        CALL OFView_Reset()
 	
 ! Pause time at the beginning
 	!CALL OFFrame_ManageTime(.true., .false., .true., .true., .true., 0.0d0, .false., 1.0D0)
 	
-! Create a FrameManager which will handle a ReferenceFrame heirarchy
+! Create a FrameManager to handle a ReferenceFrame heirarchy
 	CALL OFFM_Create(0)
 	CALL OFFM_SetFrame()
 
@@ -92,21 +120,21 @@
 	CALL OFWin_SetSwapBuffersFunction(SwapBuffers)
 	CALL OFWin_SetMakeCurrentFunction(MakeCurrent)
 		
-	!CALL OFWin_SetBackgroundTexture(0, 0, "../Images/StarMap.tif")
+	CALL OFWin_SetBackgroundTexture(0, 0, "../Images/StarMap.tif")
 
 	CALL OFWin_SetScene(0, 0)
 
-	    T = 0.0D0
-	    DO WHILE(T < 2.0D0*PI)
-	      X = DSIN(T)
-	      Y = DCOS(T)
-	      Z = 0.0D0
+	T = 0.0D0
+	DO WHILE(T < 2.0D0*PI)
+	  X = 10000.0d0*DSIN(T)
+	  Y = 10000.0d0*DCOS(T)
+	  Z = 0.0D0
 
-	      CALL OFTraj_AddTime(T)
-	      CALL OFTraj_AddPosition(X, Y, Z)
+	  CALL OFTraj_AddTime(T*10.0d0)
+	  CALL OFTraj_AddPosition(X, Y, Z)
 
-	      T = T + PI/360.0D0
-	    END DO
+	  T = T + PI/180.0D0
+	END DO
 		
 ! Start animation
 	CALL OFWin_Start()
@@ -115,7 +143,6 @@
 	CALL WMessageEnable(MouseButUp,1)
 	CALL WMessageEnable(MouseMove, 1)
 	DO
-	    !CALL WMessagePeek(ITYPE, MESSAGE)
 	    CALL WMessage(ITYPE, MESSAGE)
 	    SELECT CASE (ITYPE)
 		CASE (MouseButDown)
@@ -148,7 +175,7 @@
 	    	    
 	END DO
 !
-!  Terminate Open-GL and Winteracter
+!  Terminate OpenGL and Winteracter
 !
 	CALL WglSelect(0)
 	CALL WindowClose()
