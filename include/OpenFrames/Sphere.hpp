47416726db1da68f5c7e6b855c5f32e2ad432625
Ravi Mathur <ravi.mathur@emergentspace.com>
2017-07-27 18:59:53 -0400

    Sphere correctly reports its bounds Fixed indenting

--- include/OpenFrames/Sphere.hpp
+++ include/OpenFrames/Sphere.hpp
@@ -1,5 +1,5 @@
 /***********************************
-   Copyright 2013 Ravishankar Mathur
+   Copyright 2017 Ravishankar Mathur
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -25,52 +25,52 @@
 namespace OpenFrames
 {
 
-/*******************************************************************
- * Ravi Mathur
- * OpenFrames API, class Sphere
- * A Sphere is a ReferenceFrame with a 3D model of a sphere at its origin.
- * The radius of the sphere is adjustable, and a texture can be mapped
- * onto the sphere if desired.
-******************************************************************/
-class OF_EXPORT Sphere : public ReferenceFrame
-{
-  public:
-	Sphere( const std::string &name );
-	Sphere( const std::string &name, const osg::Vec3 &color );
-	Sphere( const std::string &name, const osg::Vec4 &color );
-	Sphere( const std::string &name , float r, float g, float b, float a = 1.0 );
+  /*******************************************************************
+   * Ravi Mathur
+   * OpenFrames API, class Sphere
+   * A Sphere is a ReferenceFrame with a 3D model of a sphere at its origin.
+   * The radius of the sphere is adjustable, and a texture can be mapped
+   * onto the sphere if desired.
+   ******************************************************************/
+  class OF_EXPORT Sphere : public ReferenceFrame
+  {
+    public:
+      Sphere( const std::string &name );
+      Sphere( const std::string &name, const osg::Vec3 &color );
+      Sphere( const std::string &name, const osg::Vec4 &color );
+      Sphere( const std::string &name , float r, float g, float b, float a = 1.0 );
 
-	/** Set the radius of the sphere, given wrt the origin of
-	    the sphere's reference frame */
-	void setRadius( const double &radius );
-	double getRadius();
+      /** Set the radius of the sphere, given wrt the origin of
+        the sphere's reference frame */
+      void setRadius( const double &radius );
+      double getRadius() const;
 
-	/** Set the filename of the texture to map onto the sphere.  Consult the
-	    osg documentation to see which image filetypes are supported. */
-	bool setTextureMap( const std::string &fname, bool force_reload = false );
+      /** Set the filename of the texture to map onto the sphere.  Consult the
+        osg documentation to see which image filetypes are supported. */
+      bool setTextureMap( const std::string &fname, bool force_reload = false );
 
-	/** Have the sphere automatically adjust its detail level depending on
-	    how far it is from the current eye point. */
-	void setAutoLOD( bool lod );
-	
-	/** Inherited from ReferenceFrame.
-	    Set the color of the sphere.  If a texture is applied, the color is
-	    used to filter the texture. */
-	virtual void setColor( const osg::Vec4 &color );
-	using ReferenceFrame::setColor; // Unhide other setColor() functions
-	
-	/** Inherited from ReferenceFrame. */
-	virtual const osg::BoundingSphere& getBound() const;
+      /** Have the sphere automatically adjust its detail level depending on
+        how far it is from the current eye point. */
+      void setAutoLOD( bool lod );
 
-  protected:
-	virtual ~Sphere();
+      /** Inherited from ReferenceFrame.
+        Set the color of the sphere.  If a texture is applied, the color is
+        used to filter the texture. */
+      virtual void setColor( const osg::Vec4 &color );
+      using ReferenceFrame::setColor; // Unhide other setColor() functions
 
-	osg::ref_ptr<osg::Geode> _geode; // Node containing the sphere
-        osg::ref_ptr<osg::ShapeDrawable> _sphereSD; // The actual sphere
+      /** Inherited from ReferenceFrame. */
+      virtual const osg::BoundingSphere& getBound() const;
 
-  private:
-        void _init();
-};
+    protected:
+      virtual ~Sphere();
+
+      osg::ref_ptr<osg::Geode> _geode; // Node containing the sphere
+      osg::ref_ptr<osg::ShapeDrawable> _sphereSD; // The actual sphere
+
+    private:
+      void _init();
+  };
 
 } // !namespace OpenFrames
 
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,5 +1,5 @@
 /***********************************
-   Copyright 2013 Ravishankar Mathur
+   Copyright 2017 Ravishankar Mathur
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -28,204 +28,210 @@
 namespace OpenFrames
 {
 
-/** A cull callback that sets the detail level of a Sphere based on how far
+  /** A cull callback that sets the detail level of a Sphere based on how far
     away it is from the eye point. */
-struct SphereLODCallback : public osg::DrawableCullCallback
-{
-	SphereLODCallback(const osg::Sphere& sphere, osg::TessellationHints& hints) 
-	: _sphere(sphere), _hints(hints), _currentLOD(0) {}
+  struct SphereLODCallback : public osg::DrawableCullCallback
+  {
+    SphereLODCallback(const osg::Sphere& sphere, osg::TessellationHints& hints) 
+      : _sphere(sphere), _hints(hints), _currentLOD(0) {}
 
-	virtual bool cull(osg::NodeVisitor* nv, osg::Drawable* drawable, osg::RenderInfo* renderInfo) const 
-	{
-	  osgUtil::CullVisitor *cv = dynamic_cast<osgUtil::CullVisitor*>(nv);
-	  if(cv)
-	  {
-	    // Get the eye point in the Sphere's local coordinates
-            // Can't use cv->getEyeLocal() since Vec3=Vec3f
-	    osg::Vec3d eye = osg::Matrix::inverse(*cv->getModelViewMatrix()).getTrans();
+    virtual bool cull(osg::NodeVisitor* nv, osg::Drawable* drawable, osg::RenderInfo* renderInfo) const 
+    {
+      osgUtil::CullVisitor *cv = dynamic_cast<osgUtil::CullVisitor*>(nv);
+      if(cv)
+      {
+        // Get the eye point in the Sphere's local coordinates
+        // Can't use cv->getEyeLocal() since Vec3=Vec3f
+        osg::Vec3d eye = osg::Matrix::inverse(*cv->getModelViewMatrix()).getTrans();
 
-	    // Compute the ratio of distance to sphere radius
-	    double ratio = (eye - _sphere.getCenter()).length() / _sphere.getRadius();
-	    int lod;
-	    if(ratio <= 1.0) lod = 50;
-	    else if(ratio >= 5.0) lod = 10;
-	    else lod = 50.0/ratio;
+        // Compute the ratio of distance to sphere radius
+        double ratio = (eye - _sphere.getCenter()).length() / _sphere.getRadius();
+        int lod;
+        if(ratio <= 1.0) lod = 50;
+        else if(ratio >= 5.0) lod = 10;
+        else lod = 50.0/ratio;
 
-	    if(_currentLOD != lod)
-	    {
-	      _hints.setDetailRatio(0.1*(double)lod);
-	      drawable->dirtyDisplayList();
-	      _currentLOD = lod;
-	    }
-	  }
+        if(_currentLOD != lod)
+        {
+          _hints.setDetailRatio(0.1*(double)lod);
+          drawable->dirtyDisplayList();
+          _currentLOD = lod;
+        }
+      }
 
-	  return false; 
-	}
+      return false; 
+    }
 
-	const osg::Sphere& _sphere;
-	osg::TessellationHints& _hints;
-	mutable int _currentLOD;
-};
+    const osg::Sphere& _sphere;
+    osg::TessellationHints& _hints;
+    mutable int _currentLOD;
+  };
 
-Sphere::Sphere(const std::string &name)
-	: ReferenceFrame(name)
-{
-	_init();
-}
+  Sphere::Sphere(const std::string &name)
+    : ReferenceFrame(name)
+  {
+    _init();
+  }
 
-Sphere::Sphere(const std::string &name, const osg::Vec3 &color)
-	: ReferenceFrame(name, color)
-{
-	_init();
-}
+  Sphere::Sphere(const std::string &name, const osg::Vec3 &color)
+    : ReferenceFrame(name, color)
+  {
+    _init();
+  }
 
-Sphere::Sphere(const std::string &name, const osg::Vec4 &color)
-	: ReferenceFrame(name, color)
-{
-	_init();
-}
+  Sphere::Sphere(const std::string &name, const osg::Vec4 &color)
+    : ReferenceFrame(name, color)
+  {
+    _init();
+  }
 
-Sphere::Sphere(const std::string &name, float r, float g, float b, float a)
-	: ReferenceFrame(name, r, g, b, a)
-{
-	_init();
-}
+  Sphere::Sphere(const std::string &name, float r, float g, float b, float a)
+    : ReferenceFrame(name, r, g, b, a)
+  {
+    _init();
+  }
 
-Sphere::~Sphere() { }
+  Sphere::~Sphere() { }
 
-/** Create the sphere with default radius = 1,
+  /** Create the sphere with default radius = 1,
     and color = the color of the sphere's reference frame. */
-void Sphere::_init()
-{
-	_sphereSD = new osg::ShapeDrawable;
-        _sphereSD->setName("SphereDrawable");
-	osg::Sphere* sphere = new osg::Sphere;
-	osg::TessellationHints* hints = new osg::TessellationHints;
+  void Sphere::_init()
+  {
+    _sphereSD = new osg::ShapeDrawable;
+    _sphereSD->setName("SphereDrawable");
+    osg::Sphere* sphere = new osg::Sphere;
+    osg::TessellationHints* hints = new osg::TessellationHints;
 
-	// Set the shape to be drawn
-	_sphereSD->setShape(sphere);
-	_sphereSD->setTessellationHints(hints);
+    // Set the shape to be drawn
+    _sphereSD->setShape(sphere);
+    _sphereSD->setTessellationHints(hints);
 
-	// Don't need very high detail on a simple sphere
-	hints->setDetailRatio(2.0);
+    // Don't need very high detail on a simple sphere
+    hints->setDetailRatio(2.0);
 
-	// Offset the sphere's polygons backwards so that a decal on the sphere's
-	// surface will always show up on top of the sphere.
-	osg::PolygonOffset *offset = new osg::PolygonOffset(1, 1);
-	_sphereSD->getOrCreateStateSet()->setAttributeAndModes(offset);
+    // Offset the sphere's polygons backwards so that a decal on the sphere's
+    // surface will always show up on top of the sphere.
+    osg::PolygonOffset *offset = new osg::PolygonOffset(1, 1);
+    _sphereSD->getOrCreateStateSet()->setAttributeAndModes(offset);
 
-	// Create the node that contains the Sphere
-	_geode = new osg::Geode;
-	_geode->setName(_name);
-	_geode->addDrawable(_sphereSD);
-	
-	// Add the sphere to the ReferenceFrame
-	_xform->addChild(_geode.get());
+    // Create the node that contains the Sphere
+    _geode = new osg::Geode;
+    _geode->setName(_name);
+    _geode->addDrawable(_sphereSD);
 
-	// Resize the axes vectors and set the appropriate color
-	setRadius(1.0);
-	setColor(getColor());
-}
+    // Add the sphere to the ReferenceFrame
+    _xform->addChild(_geode.get());
 
-void Sphere::setRadius(const double &radius)
-{
-	osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
-	sphere->setRadius(radius);
-	_sphereSD->dirtyBound();
+    // Resize the axes vectors and set the appropriate color
+    setRadius(1.0);
+    setColor(getColor());
+  }
 
-	moveXAxis(osg::Vec3(radius, 0, 0), 0.5*radius);
-	moveYAxis(osg::Vec3(0, radius, 0), 0.5*radius);
-	moveZAxis(osg::Vec3(0, 0, radius), 0.5*radius);
-}
+  void Sphere::setRadius(const double &radius)
+  {
+    osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
+    sphere->setRadius(radius);
+    _sphereSD->dirtyBound();
 
-double Sphere::getRadius()
-{
-	osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
-	return sphere->getRadius();
-}
+    moveXAxis(osg::Vec3(radius, 0, 0), 0.5*radius);
+    moveYAxis(osg::Vec3(0, radius, 0), 0.5*radius);
+    moveZAxis(osg::Vec3(0, 0, radius), 0.5*radius);
+  }
 
-bool Sphere::setTextureMap(const std::string &fname, bool force_reload)
-{
-	if(fname.length() == 0) // Remove existing texture
-	{
-	  osg::StateSet* stateset = _sphereSD->getStateSet();
-	  if(stateset)
-	  {
-	    stateset->removeTextureAttribute(0, osg::StateAttribute::TEXTURE);
-	    stateset->removeTextureAttribute(0, osg::StateAttribute::TEXENV);
-	  }
+  double Sphere::getRadius() const
+  {
+    osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
+    return sphere->getRadius();
+  }
 
-	  return false;
-	}
+  bool Sphere::setTextureMap(const std::string &fname, bool force_reload)
+  {
+    if(fname.length() == 0) // Remove existing texture
+    {
+      osg::StateSet* stateset = _sphereSD->getStateSet();
+      if(stateset)
+      {
+        stateset->removeTextureAttribute(0, osg::StateAttribute::TEXTURE);
+        stateset->removeTextureAttribute(0, osg::StateAttribute::TEXENV);
+      }
 
-	// Check if there is already a texture being used.
-	osg::StateSet* stateset = _sphereSD->getOrCreateStateSet();
-	osg::Texture2D* texture = dynamic_cast<osg::Texture2D*>(stateset->getTextureAttribute(0, osg::StateAttribute::TEXTURE));
-	
-	// If the current texture has the same filename as the new texture, then reload only if we have to.
-	if(texture && (texture->getImage()->getFileName() == fname) && !force_reload) return true;
+      return false;
+    }
 
-	osg::Image* image = osgDB::readImageFile(fname);
-	if(image)
-	{
-	  // Create texture using image, and make sure it wraps around the
-	  // sphere without a seam at the edges.
-	  texture = new osg::Texture2D;
-	  texture->setImage(image);
-	  texture->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
-	  texture->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+    // Check if there is already a texture being used.
+    osg::StateSet* stateset = _sphereSD->getOrCreateStateSet();
+    osg::Texture2D* texture = dynamic_cast<osg::Texture2D*>(stateset->getTextureAttribute(0, osg::StateAttribute::TEXTURE));
 
-	  // Don't use the sphere's color when mapping the texture.
-	  osg::TexEnv* texenv = new osg::TexEnv;
-	  texenv->setMode(osg::TexEnv::DECAL);
+    // If the current texture has the same filename as the new texture, then reload only if we have to.
+    if(texture && (texture->getImage()->getFileName() == fname) && !force_reload) return true;
 
-	  // Set the texture to the sphere
-	  stateset->setTextureAttributeAndModes(0, texture);
-	  stateset->setTextureAttribute(0, texenv);
+    osg::Image* image = osgDB::readImageFile(fname);
+    if(image)
+    {
+      // Create texture using image, and make sure it wraps around the
+      // sphere without a seam at the edges.
+      texture = new osg::Texture2D;
+      texture->setImage(image);
+      texture->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+      texture->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
 
-	  return true;
-	}
-	else
-	{
-	  std::cerr<< "Sphere::setTextureMap ERROR: File \'" << fname 
-	           << "\' could not be loaded." << std::endl;
-	  return false;
-	}
-}
+      // Don't use the sphere's color when mapping the texture.
+      osg::TexEnv* texenv = new osg::TexEnv;
+      texenv->setMode(osg::TexEnv::DECAL);
 
-void Sphere::setAutoLOD( bool lod )
-{
-	if(lod)
-	{
-	  osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
-	  osg::TessellationHints *hints = static_cast<osg::TessellationHints*>(_sphereSD->getTessellationHints());
+      // Set the texture to the sphere
+      stateset->setTextureAttributeAndModes(0, texture);
+      stateset->setTextureAttribute(0, texenv);
 
-	  if(_sphereSD->getCullCallback() == NULL)
-	    _sphereSD->setCullCallback(new SphereLODCallback(*sphere, *hints));
-	}
-	else
-	{
-	  _sphereSD->setCullCallback(NULL);
-	}
-}
+      return true;
+    }
+    else
+    {
+      std::cerr<< "Sphere::setTextureMap ERROR: File \'" << fname 
+        << "\' could not be loaded." << std::endl;
+      return false;
+    }
+  }
 
-void Sphere::setColor( const osg::Vec4 &color )
-{
-	ReferenceFrame::setColor(color);
-        _sphereSD->setColor(color);
-}
+  void Sphere::setAutoLOD( bool lod )
+  {
+    if(lod)
+    {
+      osg::Sphere *sphere = static_cast<osg::Sphere*>(_sphereSD->getShape());
+      osg::TessellationHints *hints = static_cast<osg::TessellationHints*>(_sphereSD->getTessellationHints());
 
-const osg::BoundingSphere& Sphere::getBound() const
-{
-	// Have bounding sphere encompass sphere and axes/labels, but centered
-	// on the sphere (since that is the object of interest)
-	ReferenceFrame::getBound();
-	osg::BoundingSphere bs = _geode->getBound();
-	bs.expandRadiusBy(_bound);
-	_bound = bs;
+      if(_sphereSD->getCullCallback() == NULL)
+        _sphereSD->setCullCallback(new SphereLODCallback(*sphere, *hints));
+    }
+    else
+    {
+      _sphereSD->setCullCallback(NULL);
+    }
+  }
 
-	return _bound;
-}
+  void Sphere::setColor( const osg::Vec4 &color )
+  {
+    ReferenceFrame::setColor(color);
+    _sphereSD->setColor(color);
+  }
+
+  const osg::BoundingSphere& Sphere::getBound() const
+  {
+    // Normally we would just get the Sphere's bound by computing its
+    // Geode's bound. However, Geode's bound is computed by fitting
+    // a bounding sphere to its bounding box, which makes the bound
+    // bigger than the specified Sphere radius. So we will just use
+    // the center from the Geode's bound, and set the radius ourselves.
+    osg::BoundingSphere bs = _geode->getBound();
+    bs._radius = getRadius();
+
+    // Keep bound center but expand to include axes/labels
+    ReferenceFrame::getBound();
+    bs.expandRadiusBy(_bound);
+    _bound = bs;
+
+    return _bound;
+  }
 
 } // !namespace OpenFrames
 
