7a4e8ebe4741a0a002363534fe5d20601de184df
Ravi Mathur <ravi.mathur@emergentspace.com>
2020-10-08 10:18:31 -0400

    Updated PyOF RPATHs on Mac Converted basic of_test Python example to use PyOF

--- Demos/Python/of_test.py
+++ Demos/Python/of_test.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Simple OFInterface test
+Simple PyOF example
 
 """
 
-from OpenFrames.PyOFInterfaceC import *
+import OpenFrames.PyOF as PyOF
 
 
-win_id = 1
-fm_id = 2
-frame_name = "Origin"
+# Create a reference frame for the scene root
+root = PyOF.CoordinateAxes("Origin")
 
-ofwin_createproxy(50, 50, 800, 600, 1, 1, False, win_id, False)
+# Create a frame manager to control access to the scene
+fm = PyOF.FrameManager(root)
 
-ofcoordaxes_create(frame_name)
-
-offm_create(fm_id)
-offm_setframe()
-
-ofwin_setscene(0, 0)
-
-ofwin_start()
-
-ofwin_waitforstop()
+# Setup a window into which the scene will be rendered
+myWindow = PyOF.WindowProxy(50, 50, 800, 600, 1, 1, False, False)
+myWindow.setScene(fm, 0, 0)
+myWindow.startThread();
+myWindow.join();
